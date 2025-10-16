117b95b6ef83565322aa1762c7ace4ec74975f02
codyzeyn <codyzeyn@gmail.com>
2025-10-16 14:52:59 +0800

    Add lottery game simulation
    Implemented a lottery game simulation where users guess a randomly generated number between 1 and 50.

---
+++ lotterygame_ver2.0.py
@@ -0,0 +1,25 @@
+import random as ran
+import time
+print("< lottery game simulation !!! >")
+print("")
+a = input("are you ready? yes/no --> ").lower()
+if a == "yes":
+    print("the lucky winner number is between 1-50")
+    print("")
+    input("press enter to start....")
+    while True:
+        randnum = ran.randint(1, 50)
+        for letter in ["processing", ".", ".", ".", ".", "."]:
+            print(letter, end="", flush=True)
+            time.sleep(0.5)
+        print()
+        x = input("guess the number: ")
+        print("the lucky number is: ", randnum)
+        if randnum == x:
+            print("You won!!")
+            break
+        else:
+            print("you lost, try again.")
+            x = input("press enter to try again... otherwise type something --> ")
+            if x != "":
+                break
