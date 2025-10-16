5d06cf447c04e2d4fb8d07cd681788146991149a
codyzeyn <codyzeyn@gmail.com>
2025-10-16 15:53:18 +0800

    Create 5number_averagecal.py

---
+++ 5number_averagecal.py
@@ -0,0 +1,9 @@
+print("Average of the 5 number calculator")
+print("")
+a = int(input("first num: "))
+b = int(input("second num: "))
+c = int(input("third num: "))
+d = int(input("fourth num: "))
+e = int(input("fifth num: "))
+average_sum = int(a + b + c + d + e) / 5
+print("The average is:", average_sum)
